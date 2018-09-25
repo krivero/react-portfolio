daed0e48e32469b79dbb28cce0bb3bec0f11f4cc
Kristina Rivero <kristinarivero@gmail.com>
2018-09-25 13:59:54 -0400

    format projects page

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,5 +1,17 @@
 import React, { Component } from "react";
-import { Tabs, Tab } from "react-mdl";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardText,
+  CardActions,
+  CardMenu,
+  Button,
+  IconButton
+} from "react-mdl";
 
 class Projects extends Component {
   constructor(props) {
@@ -9,7 +21,31 @@ class Projects extends Component {
 
   toggleCategories() {
     if (this.state.activeTab === 0) {
-      return <div>This is Ruby on Rails</div>;
+      return (
+        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "white",
+              height: "176px",
+              background:
+                "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center/cover"
+            }}
+          >
+            United Way Project
+          </CardTitle>
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            sagittis pellentesque lacus eleifend lacinia...
+          </CardText>
+          <CardActions border>
+            <Button colored>GitHub</Button>
+            <Button colored>Live Demo</Button>
+          </CardActions>
+          <CardMenu style={{ color: "white" }}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+      );
     } else if (this.state.activeTab === 1) {
       return <div>This is React</div>;
     } else if (this.state.activeTab === 2) {
@@ -29,7 +65,13 @@ class Projects extends Component {
           <Tab>React</Tab>
           <Tab>JavaScript</Tab>
         </Tabs>
-        <section className="projects-grid">{this.toggleCategories()}</section>
+        <section className="projects-grid">
+          <Grid className="projects-grid">
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
+        </section>
       </div>
     );
   }
