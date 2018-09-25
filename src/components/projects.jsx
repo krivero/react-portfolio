a2f08e24ba85264872efdff5e7eaa8ff1b3c117f
Kristina Rivero <kristinarivero@gmail.com>
2018-09-25 19:25:00 -0400

    add project cards and style to project section

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -22,34 +22,232 @@ class Projects extends Component {
   toggleCategories() {
     if (this.state.activeTab === 0) {
       return (
-        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
-          <CardTitle
-            style={{
-              color: "white",
-              height: "176px",
-              background:
-                "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center/cover"
-            }}
-          >
-            United Way Project
-          </CardTitle>
-          <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            sagittis pellentesque lacus eleifend lacinia...
-          </CardText>
-          <CardActions border>
-            <Button colored>GitHub</Button>
-            <Button colored>Live Demo</Button>
-          </CardActions>
-          <CardMenu style={{ color: "white" }}>
-            <IconButton name="share" />
-          </CardMenu>
-        </Card>
+        <div className="projects-grid">
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "white",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center/cover"
+              }}
+            >
+              United Way Project
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "white" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "white",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center/cover"
+              }}
+            >
+              Project #2
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "white" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "white",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center/cover"
+              }}
+            >
+              Project #3
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "white" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
       );
     } else if (this.state.activeTab === 1) {
-      return <div>This is React</div>;
+      return (
+        <div className="projects-grid">
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "white",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center/cover"
+              }}
+            >
+              Project #1
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "white" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "white",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center/cover"
+              }}
+            >
+              Project #2
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "white" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "white",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center/cover"
+              }}
+            >
+              Project #3
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "white" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      );
     } else if (this.state.activeTab === 2) {
-      return <div>This is HTML/CSS</div>;
+      return (
+        <div className="projects-grid">
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "white",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center/cover"
+              }}
+            >
+              Project #1
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "white" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "white",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center/cover"
+              }}
+            >
+              Project #2
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "white" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "white",
+                height: "176px",
+                background:
+                  "url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center/cover"
+              }}
+            >
+              Project #3
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "white" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      );
     }
   }
 
@@ -65,13 +263,12 @@ class Projects extends Component {
           <Tab>React</Tab>
           <Tab>JavaScript</Tab>
         </Tabs>
-        <section className="projects-grid">
-          <Grid className="projects-grid">
-            <Cell col={12}>
-              <div className="content">{this.toggleCategories()}</div>
-            </Cell>
-          </Grid>
-        </section>
+
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
