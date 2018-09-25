527514f4aae0efca9d01254c83948269c7adec5a
Kristina Rivero <kristinarivero@gmail.com>
2018-09-25 13:16:03 -0400

    remove jquery import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import Main from "./components/main";
 import { Link } from "react-router-dom";
-import $ from "jquery";
 
 class App extends Component {
   render() {
