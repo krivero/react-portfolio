637cb7f7c2b6d0c1392b22b44ec7bb765111f1f3
Kristina Rivero <kristinarivero@gmail.com>
2018-09-25 12:33:35 -0400

    add avatar and h1 to landing page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,90 @@ import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import Main from "./components/main";
 import { Link } from "react-router-dom";
 
+var pos = 0;
+var speed = 75;
+var str = document.getElementById("str").innerText;
+
+// empty the str from span
+document.getElementById("str").innerText = "";
+
+// typing fn
+function type() {
+  if (pos < str.length) {
+    document.getElementById("str").innerText += str.charAt(pos);
+    pos++;
+    setTimeout(type, speed); //call this fn again to type all letters
+  } else {
+    setTimeout(erase, speed);
+  }
+}
+
+// start type
+setTimeout(type, speed);
+
+// erase fn
+function erase() {
+  if (pos >= 0) {
+    var temp = str.substring(0, pos);
+    document.getElementById("str").innerText = temp;
+    pos--;
+    setTimeout(erase, speed);
+  } else {
+    // start type again
+    setTimeout(type, speed);
+  }
+}
+
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 70
+          },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 100
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+})(jQuery); // End of use strict
+
 class App extends Component {
   render() {
     return (
--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -6,7 +6,20 @@ class Landing extends Component {
     return (
       <div style={{ width: "100%", margin: "auto" }}>
         <Grid className="landing-page">
-          <Cell col={12} />
+          <Cell col={12}>
+            <img
+              src="https://www.shareicon.net/download/2016/05/26/771190_people_512x512.png"
+              alt="avatar"
+              className="avatar"
+            />
+            <div className="banner">
+              <h1>Full Stack Web Developer</h1>
+              <span class="text text-white" id="str">
+                WEB DEVELOPER
+              </span>
+              <span class="type-cursor">|</span>
+            </div>
+          </Cell>
         </Grid>
       </div>
     );
