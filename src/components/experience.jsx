b1da3655b34adf9cb32f2978da917abb884518f5
Kristina Rivero <kristinarivero@gmail.com>
2018-09-26 17:57:50 -0400

    add job title to experience component

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -11,7 +11,8 @@ class Experience extends Component {
         </Cell>
         <Cell col={8}>
           <h4 style={{ marginTop: "0px" }}>{this.props.companyName}</h4>
-          <h6 style={{ marginTop: "0px" }}>{this.props.companyCity}</h6>
+          <p style={{ marginTop: "0px" }}>{this.props.companyCity}</p>
+          <p>{this.props.jobTitle}</p>
           <p>{this.props.jobDescription}</p>
         </Cell>
       </Grid>
