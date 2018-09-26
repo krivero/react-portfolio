9866455cc9a48d1b9698727ec468f78073a766ca
Kristina Rivero <kristinarivero@gmail.com>
2018-09-26 17:10:12 -0400

    create skills component

---
+++ src/components/skills.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+class Skills extends Component {
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Skills;
