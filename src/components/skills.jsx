09f42fa607a8fbb450bb920f4f88b8fd262a4f3b
Kristina Rivero <kristinarivero@gmail.com>
2018-09-26 17:58:47 -0400

    add margin to progress bar

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,10 +1,24 @@
-import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import React, { Component } from "react";
+import { Grid, Cell, ProgressBar } from "react-mdl";
 
 class Skills extends Component {
-    render() { 
-        return (  );
-    }
+  render() {
+    return (
+      <Grid>
+        <Cell col={3}>
+          <div>{this.props.skill}</div>
+        </Cell>
+        <Cell style={{ marginTop: "1rem" }} col={9}>
+          <div>
+            <ProgressBar
+              style={{ margin: "auto", width: "100%" }}
+              progress={this.props.progress}
+            />
+          </div>
+        </Cell>
+      </Grid>
+    );
+  }
 }
- 
+
 export default Skills;
