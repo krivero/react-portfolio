f0831a82c3ede4d122c726297abd432bc4569b30
Kristina Rivero <kristinarivero@gmail.com>
2018-09-26 16:40:50 -0400

    setup and format resume component

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,10 +1,42 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class Resume extends Component {
   render() {
     return (
       <div>
-        <h1>Resume</h1>
+        <Grid>
+          <Cell col={4}>
+            <div style={{ textAlign: "center" }}>
+              <img
+                src="https://www.shareicon.net/download/2016/05/26/771190_people_512x512.png"
+                alt="avatar"
+                style={{ height: "20rem" }}
+              />
+            </div>
+            <h2 style={{ paddingTop: "2em" }}> Kristina Rivero </h2>
+            <h4 style={{ color: "grey" }}>Web Developer</h4>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+            <h5>Email</h5>
+            <p>kristinarivero@gmail.com</p>
+            <h5>Web</h5>
+            <p>kristinarivero.com</p>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+          </Cell>
+          <Cell className="resume-right-col" col={8}>
+            Right Side
+          </Cell>
+        </Grid>
       </div>
     );
   }
