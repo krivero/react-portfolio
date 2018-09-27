4d4e3e99927baa7c0abe7846dc62481a53294dda
Kristina Rivero <kristinarivero@gmail.com>
2018-09-26 20:06:31 -0400

    add social links

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,10 +1,43 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class Contact extends Component {
   render() {
     return (
       <div>
-        <h1>Contact</h1>
+        <Grid>
+          <Cell
+            style={{ color: "black" }}
+            className="contact"
+            col={8}
+            offset={2}
+          >
+            <h2>Contact</h2>
+            <div>
+              <a
+                href="https://www.linkedin.com/in/kristina-rivero/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="fa fa-linkedin-square" aria-hidden="true" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/kristina-rivero/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="fa fa-github-square" aria-hidden="true" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/kristina-rivero/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="fa fa-twitter-square" aria-hidden="true" />
+              </a>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
