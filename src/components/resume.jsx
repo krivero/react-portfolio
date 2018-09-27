bbd34a60969e15354751ee01a602e0d455f65abc
Kristina Rivero <kristinarivero@gmail.com>
2018-09-26 20:37:35 -0400

    format and style resume

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -18,30 +18,54 @@ class Resume extends Component {
                 style={{ height: "20rem" }}
               />
             </div>
-            <h2 style={{ paddingTop: "2em" }}> Kristina Rivero </h2>
-            <h4 style={{ color: "grey" }}>Web Developer</h4>
-            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
-            <h5>Email</h5>
-            <p>kristinarivero@gmail.com</p>
-            <h5>Web</h5>
-            <p>kristinarivero.com</p>
-            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+            <div style={{ textAlign: "center" }}>
+              <h2 style={{ paddingTop: ".5em" }}> Kristina Rivero </h2>
+              <h4>Web Developer</h4>
+              <hr
+                style={{
+                  borderTop: "3px solid gray",
+                  width: "50%",
+                  margin: "auto"
+                }}
+              />
+              <p style={{ marginTop: "1rem" }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <hr
+                style={{
+                  borderTop: "3px solid gray",
+                  width: "50%",
+                  margin: "auto"
+                }}
+              />
+              <h5>Email</h5>
+              <p>kristinarivero@gmail.com</p>
+              <h5>Web</h5>
+              <p>kristinarivero.com</p>
+              <hr
+                style={{
+                  borderTop: "3px solid gray",
+                  width: "50%",
+                  margin: "auto"
+                }}
+              />
+            </div>
           </Cell>
 
           <Cell className="resume-right-col" col={8}>
-            <a href={pdf} target="_blank">
-              Click here for PDF Version
-            </a>
+            <div style={{ textAlign: "right" }}>
+              <button className="pdfButton">
+                <a href={pdf} target="_blank">
+                  Click here for PDF Version
+                </a>
+              </button>
+            </div>
             <h2>Education</h2>
             <Education
               endDate={2018}
