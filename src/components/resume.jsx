93553548d66ec23b0e53413dfd0098ae34d9a6e3
Kristina Rivero <kristinarivero@gmail.com>
2018-09-27 17:21:11 -0400

    format and fill in resume information

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
+import headshot from "./headshot.jpg";
 import Experience from "./experience";
 import Education from "./education";
 import Skills from "./skills";
@@ -12,11 +13,9 @@ class Resume extends Component {
         <Grid>
           <Cell col={4}>
             <div style={{ textAlign: "center" }}>
-              <img
-                src="https://www.shareicon.net/download/2016/05/26/771190_people_512x512.png"
-                alt="avatar"
-                style={{ height: "20rem" }}
-              />
+              <div className="headshot-img">
+                <img src={headshot} alt="avatar" className="headshot" />
+              </div>
             </div>
             <div style={{ textAlign: "center" }}>
               <h2 style={{ paddingTop: ".5em" }}> Kristina Rivero </h2>
@@ -28,33 +27,10 @@ class Resume extends Component {
                   margin: "auto"
                 }}
               />
-              <p style={{ marginTop: "1rem" }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-              <hr
-                style={{
-                  borderTop: "3px solid gray",
-                  width: "50%",
-                  margin: "auto"
-                }}
-              />
               <h5>Email</h5>
               <p>kristinarivero@gmail.com</p>
               <h5>Web</h5>
               <p>kristinarivero.com</p>
-              <hr
-                style={{
-                  borderTop: "3px solid gray",
-                  width: "50%",
-                  margin: "auto"
-                }}
-              />
             </div>
           </Cell>
 
@@ -62,37 +38,10 @@ class Resume extends Component {
             <div style={{ textAlign: "right" }}>
               <button className="pdfButton">
                 <a href={pdf} target="_blank">
-                  Click here for PDF Version
+                  PDF Version
                 </a>
               </button>
             </div>
-            <h2>Education</h2>
-            <Education
-              endDate={2018}
-              schoolName={"Wyncode Academy"}
-              schoolDescription={"diuygvfvh"}
-            />
-            <Education
-              endDate={2014}
-              schoolName={"Florida State University"}
-              degree={"B.S. Exercise Science"}
-              schoolDescription={"diuygvfvh"}
-            />
-            <Education
-              endDate={2014}
-              schoolName={"Florida State University"}
-              degree={"B.S. Geography, Environmental Studies"}
-              schoolDescription={"diuygvfvh"}
-            />
-            <h2>Experience</h2>
-            <Experience
-              startDate={"December 2016"}
-              endDate={"June 2018"}
-              companyName={"University of Miami"}
-              companyCity={"Miami, FL"}
-              jobTitle={"Administrative Assistant/Fellowship Coordinator"}
-              jobDescription={""}
-            />
             <h2>Tech Skills</h2>
             <Skills skill={"HTML/CSS"} progress={80} />
             <Skills skill={"JavaScript"} progress={70} />
@@ -102,9 +51,169 @@ class Resume extends Component {
             <Skills skill={"Ruby on Rails"} progress={75} />
 
             <h2>Other Relevant Skills</h2>
-            <Skills skill={"Willingness to Learn"} progress={100} />
+            <Skills
+              skill={"Multitask High-Volume Environment"}
+              progress={100}
+            />
             <Skills skill={"Customer Service"} progress={100} />
             <Skills skill={"Plan and Prioritize Work"} progress={100} />
+            <h2>Education</h2>
+            <Education
+              endDate={2018}
+              schoolName={"Wyncode Academy"}
+              schoolDescription={"Full Stack Web Developer Program"}
+            />
+            <Education
+              endDate={2014}
+              schoolName={"Florida State University"}
+              degree={"B.S. Exercise Science"}
+              schoolDescription={"Tallahassee, FL"}
+            />
+            <Education
+              endDate={2014}
+              schoolName={"Florida State University"}
+              degree={"B.S. Geography, Environmental Studies"}
+              schoolDescription={"Tallahassee, FL"}
+            />
+            <h2>Experience</h2>
+            <Experience
+              startDate={"December 2016"}
+              endDate={"June 2018"}
+              companyName={"University of Miami"}
+              companyCity={"Miami, FL"}
+              jobTitle={"Administrative Assistant/Fellowship Coordinator"}
+              jobDescription={
+                <ul>
+                  <li>
+                    Managed and administered daily operations of two Orthopaedic
+                    Trauma Surgeons, including the Associate Chairman for
+                    Clinical Affairs/Chief of Trauma Service
+                  </li>
+                  <li>
+                    Served as Trauma Fellowship Coordinator (set-up meetings and
+                    interviews, grant submission)
+                  </li>
+                  <li>
+                    Collected and researched both financial and incoming trauma
+                    patient data daily; prepared reports by collecting and
+                    analyzing information
+                  </li>
+                </ul>
+              }
+            />
+            <Experience
+              startDate={"July 2016"}
+              endDate={"November 2018"}
+              companyName={"Miami Bakery"}
+              companyCity={"Miami, FL"}
+              jobTitle={"Office Manager"}
+              jobDescription={
+                <ul>
+                  <li>
+                    Managed employee schedules, organized office functions and
+                    oversaw daily operations
+                  </li>
+                  <li>
+                    Reduced office expenses by 15% by communicating with vendors
+                    and implementing inventory system
+                  </li>
+                  <li>
+                    Developed procedures and training materials to streamline
+                    the onboarding process and set clear employee expectations
+                  </li>
+                </ul>
+              }
+            />
+            <Experience
+              startDate={"February 2016"}
+              endDate={"July 2018"}
+              companyName={"Universal Engineering Sciences"}
+              companyCity={"Miami, FL"}
+              jobTitle={"Office Manager"}
+              jobDescription={
+                <ul>
+                  <li>
+                    Day-to-day running of the office at the administrative level
+                    (trained, supervised and assigned work to 3+employees);
+                    maintained uniform correspondence procedures and calendar
+                    management
+                  </li>
+                  <li>
+                    Maintained office productivity through proficient use of
+                    appropriate software applications
+                  </li>
+                  <li>
+                    Billed, invoiced and collected (upwards of $250k monthly)
+                  </li>
+                </ul>
+              }
+            />
+            <Experience
+              startDate={"June 2015"}
+              endDate={"November 2015"}
+              companyName={"Wreckless Apparel"}
+              companyCity={"Tallahassee, FL"}
+              jobTitle={"Office Assistant"}
+              jobDescription={
+                <ul>
+                  <li>
+                    Served as Interim Office Manager; primary point of contact.
+                    Assessed customers' needs and explained services, processes,
+                    procedures and guidelines
+                  </li>
+                  <li>
+                    Managed calendar of events as well as daily operations;
+                    Consistently met deadlines
+                  </li>
+                  <li>Created social media marketing material and forms</li>
+                </ul>
+              }
+            />
+            <Experience
+              startDate={"May 2014"}
+              endDate={"May 2015"}
+              companyName={"Moonwater Pictures"}
+              companyCity={"Miami, FL"}
+              jobTitle={"Associate Producer"}
+              jobDescription={
+                <ul>
+                  <li>
+                    Assisted in production logistics and daily operations;
+                    organized 10+ production personnel and wrote/edited scripts
+                    weekly
+                  </li>
+                  <li>
+                    Created shows digital content including behind the scenes
+                    footage, photography and website only video
+                  </li>
+                  <li>
+                    Consistently met strict production deadlines; 3 new sets
+                    weekly
+                  </li>
+                  <li>Show: S.O.S. Salva Mi Casa</li>
+                </ul>
+              }
+            />
+            <Experience
+              startDate={"May 2012"}
+              endDate={"January 2015"}
+              companyName={"Karma Kandy"}
+              companyCity={"Tallahassee, FL"}
+              jobTitle={"Founder/President"}
+              jobDescription={
+                <ul>
+                  <li>Created and maintained company website (HTML, CSS)</li>
+                  <li>
+                    Designed and created new products; designed all print and
+                    marketing materials
+                  </li>
+                  <li>
+                    Procured new customers (2,000+) and maintained customer
+                    relations
+                  </li>
+                </ul>
+              }
+            />
           </Cell>
         </Grid>
       </div>
